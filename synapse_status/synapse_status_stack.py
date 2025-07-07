4852f0daeadf37376f01a7a46674daef5b2a012b
Xavier Schildwachter <x.schildwachter@sagebase.org>
2025-07-07 10:39:23 -0700

    Remove commented code

--- synapse_status/synapse_status_stack.py
+++ synapse_status/synapse_status_stack.py
@@ -6,7 +6,6 @@ from aws_cdk import (
     aws_events_targets as targets,
     aws_ec2 as ec2,
 )
-from aws_cdk import aws_signer as signer
 from constructs import Construct
 from pathlib import Path
 
@@ -20,14 +19,6 @@ class SynapseStatusStack(Stack):
                  vpc_id: str,
                  **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
-
-        # # Create a signing profile
-        # signing_profile = signer.SigningProfile(self, "SigningProfile", platform=signer.Platform.AWS_LAMBDA_SHA384_ECDSA )
-        #
-        # # Create a signing config using the profile
-        # signing_config = _lambda.CodeSigningConfig(self, "SigningConfig", signing_profiles=[signing_profile],
-        #     untrusted_artifact_on_deployment=_lambda.UntrustedArtifactOnDeployment.WARN
-        # )
 
         vpc = ec2.Vpc.from_lookup(self, "SynapseVPC", vpc_id=vpc_id)
 
@@ -45,13 +36,12 @@ class SynapseStatusStack(Stack):
                 "STATUS_PAGE_IO_WEBSITE_COMPONENT_ID": statuspage_website_component_id,
             },
             timeout=Duration.seconds(30),
-            # code_signing_config=signing_config,
             vpc=vpc,
             vpc_subnets=ec2.SubnetSelection(subnet_type=ec2.SubnetType.PRIVATE_WITH_EGRESS, availability_zones=['us-east-1a', 'us-east-1c'])
         )
 
         rule = events.Rule(
             self, "StatusScheduleRule",
-            schedule=events.Schedule.rate(Duration.minutes(33)),
+            schedule=events.Schedule.rate(Duration.minutes(5)),
         )
         rule.add_target(targets.LambdaFunction(function))
